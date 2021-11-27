84f4a0b736e837db1a8b9a42df727d7fd0164914
Pedro <mendez.pedro94@gmail.com>
2021-11-27 17:15:33 -0300

    creacion del modelo comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-
+  has_many :comments, dependent: :destroy
   belongs_to :user
   belongs_to :category
   belongs_to :food_store
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_195621) do
+ActiveRecord::Schema.define(version: 2021_11_27_200945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2021_11_27_195621) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_categories_on_category_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "content"
+    t.integer "likes"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "food_stores", force: :cascade do |t|
@@ -63,6 +74,8 @@ ActiveRecord::Schema.define(version: 2021_11_27_195621) do
   end
 
   add_foreign_key "categories", "categories"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "categories"
   add_foreign_key "posts", "food_stores"
   add_foreign_key "posts", "users"
