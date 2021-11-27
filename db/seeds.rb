978e0ba18d4d600d9368ccfff432be95b0e4a08f
Pedro <mendez.pedro94@gmail.com>
2021-11-27 20:42:52 -0300

    creaccion de usuarios en el seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#elimina para no crear varios registros repetidos al ejecutar el seed
+Category.destroy_all
+User.destroy_all
+
+x=1
+5.times do
+
+    User.create(
+        email: "#{x}@gmail.com",
+        name: Faker::TvShows::Simpsons.character,
+        avatar: Faker::Avatar.image,
+        password: "123456")
+    x=x+1
+end
+
+# 6.times do
+#     Category.create(
+#         title: Faker::TvShows::Simpsons.location
+#     )
+# end
+
+# 14.times do
+#     Category.create(
+#         title: Faker::TvShows::Simpsons.location,
+#         category_id: Random.rand(1..6)
+#     )
+# end
+
