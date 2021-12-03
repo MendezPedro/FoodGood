8c9f122ff9c724890d64c750b7aa243a5586757d
Pedro <mendez.pedro94@gmail.com>
2021-12-03 15:36:50 -0300

    se agrega la opcion de editar tu perfil o eliminar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def edit
     @user = User.find(params[:id])
   end
@@ -7,7 +9,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     respond_to do |format|
       if @user.update(users_params)
-        format.js { redirect_to root_path, notice: 'urlmark create' }
+        format.js { redirect_to users_path, notice: 'datos actualizados' }
+      else
+        format.html { redirect_to root_path, alert: 'no se pudo actualizar el usuario' }
+      end
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.destroy!
+        # para que la vista no haga nada
+        #format.js { render layout: false, notice: 'lo re borraste' }
+        format.html { redirect_to root_path, notice: 'category destroy' }
       else
         format.html { redirect_to root_path, alert: 'alert url bookmarks not create' }
       end
@@ -16,6 +31,6 @@ class UsersController < ApplicationController
 
   private
   def users_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :avatar)
   end
 end
